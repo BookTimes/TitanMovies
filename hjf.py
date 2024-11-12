a3b3da3bbcbdbb23627f6612722b7a6bc4f119dd
Mahadev Maneesh <maha040963@sisharjah.com>
2024-11-12 22:38:36 +0400

    custom

--- hjf.py
+++ hjf.py
@@ -16,6 +16,50 @@ def connect_db():
     )
 userdata={'name' :''}
 
+
+
+
+def latestmovies():
+    o = connect_db()
+    k = o.cursor()
+    k.execute('select movie_title from movies order by release_date asc')
+    l = k.fetchmany(4)
+    return l
+
+def popularmovies():
+    o = connect_db()
+    k = o.cursor()
+    k.execute('select movie_title from movies order by viewers asc')
+    l = k.fetchmany(4)
+    return l
+
+def bookTicket(movie):
+    form = ctk.CTkFrame(root , fg_color='transparent')
+    form.pack(fill='both' , padx = 20 , pady=20)
+    image = Image.open("logo.png")
+    image = image.resize((135, 105))
+    logo = ImageTk.PhotoImage(image)
+    label = ctk.CTkLabel(form, image=logo, text="", anchor="w", font=("Arial", 36, 'bold'))
+    label.pack(anchor='center', padx=10, pady=5)
+    header = ctk.CTkLabel(form , text='Book Ticket' , font=('Helvetica' , 36 , 'bold'))
+    header.pack(anchor='center',padx=10)
+    personal = ctk.CTkFrame(form ,fg_color='transparent')
+    personal.pack(side='left' , padx=10)
+    name_label = ctk.CTkLabel(personal, text="Personal Information", anchor="w", font=("Arial", 15))
+    name_label.pack(fill="x", padx=2, pady=5)
+    name_input = ctk.CTkEntry(personal)
+    name_input.pack(pady=5)
+    mail = ctk.CTkFrame(form, fg_color='transparent')
+    mail.pack(side='left', padx=20)
+    mail_label = ctk.CTkLabel(mail, text="Mail id", anchor="w", font=("Arial", 15))
+    mail_label.pack(fill="x", padx=2, pady=5)
+    mail_input = ctk.CTkEntry(mail)
+    mail_input.pack(pady=5)
+
+
+
+
+
 def login():
     username = entry_username.get()
 
@@ -32,7 +76,7 @@ def login():
 
 
 # Create the main window
-root = ctk.CTk()
+root = ctk.CTk(fg_color='black')
 
 # Set window title and size
 root.title("Login Page")
@@ -64,6 +108,19 @@ login_button.pack(pady=20)
 
 
 def mainpageing():
+    def srch():
+        c = search_entry.get()
+        o = connect_db()
+        h = o.cursor()
+        h.execute(f'select movie_title from movies where movie_title="{c}"')
+        l = h.fetchone()
+        if l:
+            re = messagebox.askyesno('Found', 'Movie is available \n Would you like to book')
+            if re:
+                bookTicket(search_entry.get())
+                main_page.pack_forget()
+        else:
+            messagebox.showinfo('Not found' , "Your movie is not found")
     root.title("TitanCinema")
     main_page = ctk.CTkFrame(root,fg_color='black')
     main_page.pack(fill="x", expand=False)
@@ -80,43 +137,34 @@ def mainpageing():
     # Image label
     label_image = ctk.CTkLabel(main_page, image=photo, text=f'Welcome to TitanCinemas {userdata["name"]}',font=("Arial", 20, "bold"))
     label_image.pack(pady=(0,0))
-    search_movie_frame = ctk.CTkFrame(main_page , fg_color='black')
-    search_movie_frame.pack(fill="x", padx=20)  # Center the frame in the window
-
+    search_movie_frame = ctk.CTkFrame(main_page, fg_color='black')
+    search_movie_frame.pack(fill="x", padx=20, pady=10)  # Adjust padding around the frame
+    search_label = ctk.CTkLabel(search_movie_frame, text="Search Movies", anchor="w", font=("Arial", 36, 'bold'))
+    search_label.pack(fill="x", padx=10, pady=5)
     # Create the text box (Entry widget) for entering movie name
-    search_entry = ctk.CTkEntry(search_movie_frame, width=200, placeholder_text="Enter movie name")
-    search_entry.place(x=10, y=10)  # Positioning text box
+    search_entry = ctk.CTkEntry(search_movie_frame, width=180, placeholder_text="Enter movie name")
+    search_entry.pack(side='left' , padx=10,pady=10)  # Positioning the text box closer to the top left corner
 
     # Create the button for search
-    search_button = ctk.CTkButton(search_movie_frame, text="Search")
-    search_button.place(x=220, y=10)
+    search_button = ctk.CTkButton(search_movie_frame, text="Search", command=srch)
+    search_button.pack(side = 'left', padx=10 , pady=10)
     newfilm = ctk.CTkFrame(main_page, fg_color='transparent')
-    newfilm.pack(fill="x", padx=20, pady=20)
+    newfilm.pack(fill="x", padx=20)
     label = ctk.CTkLabel(newfilm, text="New Movies", anchor="w", font=("Arial", 36, 'bold'))
     label.pack(fill="x", padx=10, pady=5)
-    # Create Buttons and pack them horizontally
-    button1 = ctk.CTkButton(newfilm, text="ARM")
-    button1.pack(side="left", padx=10, pady=10)
+    for i in latestmovies():
+        button1 = ctk.CTkButton(newfilm, text=f"{i[0]}")
+        button1.pack(side="left", padx=10, pady=10)
 
-    button2 = ctk.CTkButton(newfilm, text="Venom")
-    button2.pack(side="left", padx=10, pady=10)
-
-    button3 = ctk.CTkButton(newfilm, text="Stree 2")
-    button3.pack(side="left", padx=10, pady=10)
 
     popularfilm = ctk.CTkFrame(main_page, fg_color='transparent')
     popularfilm.pack(fill="x", padx=20, pady=20)
     label = ctk.CTkLabel(popularfilm, text="Popular Movies", anchor="w", font=("Arial", 36, 'bold'))
     label.pack(fill="x", padx=10, pady=5)
     # Create Buttons and pack them horizontally
-    button1 = ctk.CTkButton(popularfilm, text="Pani")
-    button1.pack(side="left", padx=10, pady=10)
-
-    button2 = ctk.CTkButton(popularfilm, text="Lucky Bhaskar")
-    button2.pack(side="left", padx=10, pady=10)
-
-    button3 = ctk.CTkButton(popularfilm, text="Uljah")
-    button3.pack(side="left", padx=10, pady=10)
+    for i in popularmovies():
+        button1 = ctk.CTkButton(popularfilm, text=f"{i[0]}")
+        button1.pack(side="left", padx=10, pady=10)
 
 
 
